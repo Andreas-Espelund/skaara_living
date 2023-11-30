0c4fc7d28e3c0e49054555ce57d28cd5324723c3
Andreas Raftevold Espelund <andreres@ulrik.uio.no>
2023-11-30 19:42:31 +0100

    added shop link:

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -5,7 +5,7 @@ export default function Cabins() {
 
 
     return (
-        <div className="h-screen p-10 grid ">
+        <div className="h-screen p-10 grid playfair ">
             <a href={"/"} className={"hover:scale-125 w-fit h-fit p-2 rounded-full hover:bg-primary hover:text-black transition-all"}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
@@ -22,6 +22,13 @@ export default function Cabins() {
                     href={"https://www.airbnb.no/rooms/934184710629468152"}
                     src={"https://a0.muscache.com/im/pictures/90861d9f-e533-4d33-9751-7092062eeee5.jpg?"}
                     title={"Hjellaakeren"}/>
+
+            </div>
+
+            <div className="flex flex-col gap-10 rounded-xl">
+                <h2 className="text-4xl">Get the holiday gift of the year!</h2>
+                
+                <a className='px-8 text-black font-medium text-xl rounded-lg bg-primary w-fit py-4 hidden md:block hover:scale-105 transition-all outline-primary' href="https://shop.skaaraliving.com/" target={"_blank"}>Buy a gift card</a>
             </div>
         </div>
     )
--- components/Header.jsx
+++ components/Header.jsx
@@ -8,6 +8,7 @@ function Header() {
       <nav className='flex gap-10 text-center items-center font-semibold'>
           <a className='hidden md:block hover:scale-110 playfair transition-all outline-primary' href="#contact">CONTACT</a>
           <a className='hidden md:block hover:scale-110 playfair transition-all outline-primary' href="#location">LOCATION</a>
+          <a className='hidden md:block hover:scale-110 playfair transition-all outline-primary' href="https://shop.skaaraliving.com/" target={"_blank"}>GIFTCARD</a>
           <Link className=' hover:scale-110 text-sm md:text-md lg:text-lg transition-all box-border outline-none bg-primary rounded-full text-black px-8 py-3 ' href="/cabins">Book now</Link>
       </nav>
     </div>
